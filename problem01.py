9df1bc1569ccd024f796d6b5ae0d35cf15b81892
Alok singh <aloksingh200610@gmail.com>
2026-02-06 10:22:23 +0530

    Initial Commit

---
+++ problem01.py
@@ -0,0 +1,24 @@
+# 14. Longest Common Prefix
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+        for word in strs[1:]:
+            i = 0
+            while i < len(prefix) and i < len(word) and prefix[i] == word[i]:
+                i+= 1
+            prefix = prefix[:i]
+
+            if prefix == "":
+                break
+
+        
+        return prefix
+    
